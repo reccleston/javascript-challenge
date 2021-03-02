31f4ff8932b35a112a531cc41a6a1e61a31074e7
Ryan EM <ryan.eccleston.murdock@gmail.com>
2021-03-01 20:59:07 -0500

    added options to dropdown dynamically

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -25,5 +25,45 @@ filter_button.on('click', handleFilterSightings);
 // Enter and filter in realtime with typing (for my version)
 var input_box = d3.select('#datetime');
 
-// have filter run with each key --> type aall matches 
+// have filter run with each key --> type all matches 
 input_box.on('keyup', handleFilterSightings);
+
+// Multifiltering 
+
+// Populate Dropdowns 
+
+// getting unique values in cols
+var unique_entries = {};
+Array.prototype.contains = function(v) {
+    for (var i = 0; i < this.length; i++) {
+      if (this[i] === v) return true;
+    }
+    return false;
+};
+  
+Array.prototype.unique = function() {
+    var arr = [];
+    for (var i = 0; i < this.length; i++) {
+      if (!arr.contains(this[i])) {
+        arr.push(this[i]);
+      }
+    }
+    return arr;
+};
+
+var relevant_keys = Object.keys(tableData[0]).slice(1,5);
+
+relevant_keys.forEach(key => unique_entries[key] = tableData.map(ufo => ufo[key]).unique());
+unique_entries = Object.entries(unique_entries);
+console.log(unique_entries);
+
+// Append unique options to select drop downs
+var dropdowns = d3.selectAll('.form-select');
+dropdowns.each(function (drop, i) {
+    var options = unique_entries[i][1];
+    options.forEach(opt => d3.select(this).append('option').attr('value', `${opt}`).text(opt));
+});
+
+// Listen for change event and filter table 
+
+  
